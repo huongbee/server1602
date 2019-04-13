d517fe698c652614931d3cb2aad6ba0834a7c61b
Huong <huongnguyenak96@gmail.com>
2019-04-13 14:14:16 +0700

    createPost

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const User = require('./User')
 
 const PostSchema = new Schema({
     author : { type: Schema.Types.ObjectId, ref:'user'}, 
@@ -13,4 +14,23 @@ const PostSchema = new Schema({
 })
 const PostModel = mongoose.model('post',PostSchema)
 // create , update, delete, like, dislike
-module.exports = PostModel
+
+class Post extends PostModel{
+    static async createPost(author,content){
+        const post = await PostModel.create({author,content})
+        if(!post) throw new Error('Cannot create post!');
+        const user = User.findByIdAndUpdate(author,{
+            $addToSet:{
+                posts: post._id
+            }
+        })
+        if(!user){ 
+            //remove post
+            PostModel.findByIdAndRemove(post._id)
+            throw new Error('Cannot find author!')
+        };
+        return post;
+    }
+}
+
+module.exports = Post
--- models/User.js
+++ models/User.js
@@ -125,7 +125,7 @@ class User extends UserModel{
     }
 
     static async removeFriend(){
-        
+           
     }
 }
 
