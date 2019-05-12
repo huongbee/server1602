57a60aad35dd2382807a03d0da4722dbccb39521
Huong <huongnguyenak96@gmail.com>
2019-05-12 09:57:39 +0700

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('./lib/connectdb')
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser').urlencoded({extended:false})
+const bodyParser = require('body-parser').json();
 app.use(bodyParser)
+const cors = require('cors');
 const authenticate = require('./lib/authenticate');
 const userRouter = require('./controllers/user.route');
 const postRouter = require('./controllers/post.route');
-
+app.use(cors());
 app.use('/user',userRouter);
 app.use('/post',postRouter);
 app.listen(3000,()=>{
